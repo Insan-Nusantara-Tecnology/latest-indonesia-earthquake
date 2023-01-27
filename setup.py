6210dfa48208552d4683da84f5a647dcb3fdd3e4
Fikri Muza <fikrimuzadi@gmail.com>
2023-01-27 17:37:28 +0700

    release to pypi

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="muzadash", # Replace with your own username
-    version="0.0.1",
+    version="0.0.2",
     author="Fikri Muzadi",
     author_email="fikrimuzadi@gmail.com",
     description="This package will get the lates Dashboard Monitoring Indonesia",
